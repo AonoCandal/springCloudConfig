ecdd0868595553c2c62b59ab26ddbf719f462516
李舰舸 <lijiange@cmiot.chinamobile.com>
2022-03-12 15:39:50 +0800

    Initial commit

---
+++ src/test/java/com/aono/springcloudconfig/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aono.springcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
